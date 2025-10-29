319d4acd9a8db5c81c8182f7be1ce2ee1d273a15
ChenYJ1016 <chen_7898@hotmail.com>
2025-10-29 15:52:19 +0800

    CLEANUP: removed redundant UI

--- ios-expense-tracker/ViewController/View/ViewController/Dashboard/BudgetFormController.swift
+++ ios-expense-tracker/ViewController/View/ViewController/Dashboard/BudgetFormController.swift
@@ -22,19 +22,6 @@ class BudgetFormController: UIViewController {
     
     weak var delegate: BudgetFormControllerDelegate?
     
-    // Table View for Saving Goals
-    private let goalsTableView: UITableView = {
-        let tv = UITableView()
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.isScrollEnabled = false
-        tv.layer.cornerRadius = 8
-        // TODO: Register your custom UITableViewCell here
-        // tv.register(SavingGoalCell.self, forCellReuseIdentifier: "SavingGoalCell")
-        return tv
-    }()
-    
-    private var tableViewHeightConstraint: NSLayoutConstraint!
-    
     private let scrollView: UIScrollView = {
         let sv = UIScrollView()
         sv.translatesAutoresizingMaskIntoConstraints = false
@@ -51,37 +38,11 @@ class BudgetFormController: UIViewController {
         return sv
     }()
     
-    private let formSegmentedControl: UISegmentedControl = {
-        let sc = UISegmentedControl(items: ["Income & Savings", "Saving Goals"])
-        sc.selectedSegmentIndex = 0
-        sc.translatesAutoresizingMaskIntoConstraints = false
-        return sc
-    }()
-    
     private let overviewContainerView: UIView = {
         let v = UIView()
         v.translatesAutoresizingMaskIntoConstraints = false
         return v
     }()
-    
-    private let goalsContainerView: UIView = {
-        let v = UIView()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.isHidden = true
-        return v
-    }()
-    
-    private let saveButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Save Budget", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
-        button.backgroundColor = .systemBlue
-        button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 10
-        return button
-    }()
-    
     
     // MARK: - Lifecycle
     
@@ -93,7 +54,6 @@ class BudgetFormController: UIViewController {
         setupUI()
         setupNavigationItems()
         setupOverviewContainer()
-        setupGoalsContainer()
         setupActions()
         setupKeyboardDismissGesture()
     }
@@ -116,10 +76,7 @@ class BudgetFormController: UIViewController {
         scrollView.addSubview(contentStackView)
         
         // 3. Add components to the ContentStackView
-        contentStackView.addArrangedSubview(formSegmentedControl)
         contentStackView.addArrangedSubview(overviewContainerView)
-        contentStackView.addArrangedSubview(goalsContainerView)
-        contentStackView.addArrangedSubview(saveButton)
         
         // 4. Set constraints
         NSLayoutConstraint.activate([
@@ -139,11 +96,6 @@ class BudgetFormController: UIViewController {
             // This is key: ContentStackView's width must equal the scroll view's frame width
             // This prevents horizontal scrolling.
             contentStackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
-            
-            // Save button height
-            saveButton.heightAnchor.constraint(equalToConstant: 50)
-
-            
             
         ])
     }
@@ -165,16 +117,16 @@ class BudgetFormController: UIViewController {
         savingGoalTextField = createTextField(placeholder: "$0.00", keyboardType: .decimalPad)
         
         let remainingTitle = createTitleLabel(with: "Your budget for this month")
-        remainingAmountLabel = createAmountLabel(with: "$0.00") // Default value
+        remainingAmountLabel = createAmountLabel(with: "$0.00")
         
         // Add components to the stack view
         stackView.addArrangedSubview(incomeTitle)
         stackView.addArrangedSubview(incomeTextField)
-        stackView.setCustomSpacing(24, after: incomeTextField) // Add extra space
+        stackView.setCustomSpacing(24, after: incomeTextField)
         
         stackView.addArrangedSubview(goalTitle)
         stackView.addArrangedSubview(savingGoalTextField)
-        stackView.setCustomSpacing(24, after: savingGoalTextField) // Add extra space
+        stackView.setCustomSpacing(24, after: savingGoalTextField)
         
         stackView.addArrangedSubview(remainingTitle)
         stackView.addArrangedSubview(remainingAmountLabel)
@@ -187,97 +139,15 @@ class BudgetFormController: UIViewController {
             stackView.trailingAnchor.constraint(equalTo: overviewContainerView.trailingAnchor)
         ])
     }
-    
-    private func setupGoalsContainer() {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 16
-        
-        goalsContainerView.addSubview(stackView)
-        
-        // 1. Info Label
-        let infoLabel = createTitleLabel(with: "Breakdown Your Saving Goal")
-        infoLabel.font = .systemFont(ofSize: 18, weight: .semibold)
-        
-        // 2. Error Label (hidden by default)
-        let errorLabel = UILabel()
-        errorLabel.translatesAutoresizingMaskIntoConstraints = false
-        errorLabel.text = "Your goals don't add up to your total."
-        errorLabel.font = .systemFont(ofSize: 14)
-        errorLabel.textColor = .systemRed
-        errorLabel.isHidden = true // Show this with logic later
-        
-        // 3. Add Goal Button
-        let addGoalButton = UIButton(type: .system)
-        addGoalButton.translatesAutoresizingMaskIntoConstraints = false
-        addGoalButton.setTitle("+ Add New Goal", for: .normal)
-        addGoalButton.titleLabel?.font = .systemFont(ofSize: 17, weight: .semibold)
-        addGoalButton.backgroundColor = .systemGray5
-        addGoalButton.layer.cornerRadius = 8
-        
-        // Set up TableView
-        goalsTableView.delegate = self
-        goalsTableView.dataSource = self
-        
-        // Add components to the stack view
-        stackView.addArrangedSubview(infoLabel)
-        stackView.addArrangedSubview(errorLabel)
-        stackView.addArrangedSubview(goalsTableView)
-        stackView.addArrangedSubview(addGoalButton)
-        
-        // Pin the stackView to its container
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: goalsContainerView.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: goalsContainerView.bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: goalsContainerView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: goalsContainerView.trailingAnchor),
-            
-            addGoalButton.heightAnchor.constraint(equalToConstant: 44)
-        ])
-        
-        // Initialize the height constraint for the table view.
-        // We start it at 0. We will update this in `viewDidLayoutSubviews`.
-        tableViewHeightConstraint = goalsTableView.heightAnchor.constraint(equalToConstant: 0)
-        tableViewHeightConstraint.isActive = true
-    }
-    
-    // This method is called after the table view has its content.
-    // We update the height constraint to match the table's content size.
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        // Update height constraint
-        let newHeight = goalsTableView.contentSize.height
-        if tableViewHeightConstraint.constant != newHeight {
-            tableViewHeightConstraint.constant = newHeight
-        }
-    }
+
 
     // MARK: - Actions
     
     private func setupActions() {
-        // Add target for the segmented control
-        formSegmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
-        
-        // Add target for saveButton
-         saveButton.addTarget(self, action: #selector(savedTapped), for: .touchUpInside)
         
         // Add targets for text fields to update the 'Remaining' label
          incomeTextField.addTarget(self, action: #selector(updateRemainingBalance), for: .editingChanged)
          savingGoalTextField.addTarget(self, action: #selector(updateRemainingBalance), for: .editingChanged)
-    }
-    
-    @objc private func segmentChanged() {
-        let isOverviewSelected = formSegmentedControl.selectedSegmentIndex == 0
-        
-        UIView.animate(withDuration: 0.3) {
-            self.overviewContainerView.isHidden = !isOverviewSelected
-            self.goalsContainerView.isHidden = isOverviewSelected
-            
-            // This tells the stack view to re-layout
-            self.contentStackView.layoutIfNeeded()
-        }
     }
     
     @objc private func updateRemainingBalance() {
@@ -359,36 +229,6 @@ class BudgetFormController: UIViewController {
         return label
     }
   
-}
-
-// MARK: - UITableView Delegate & DataSource
-
-extension BudgetFormController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // TODO: Return your array of saving goals count
-        return 3 // Placeholder
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // TODO: Dequeue your custom cell
-        // let cell = tableView.dequeueReusableCell(withIdentifier: "SavingGoalCell", for: indexPath) as! SavingGoalCell
-        
-        // Placeholder cell
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
-        cell.textLabel?.text = "Goal \(indexPath.row + 1)"
-        cell.detailTextLabel?.text = "$100.00"
-        cell.accessoryType = .disclosureIndicator
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        // TODO: Present your 'Add/Edit Goal' modal view controller
-        print("Tapped on goal \(indexPath.row)")
-    }
-    
-    // TODO: Add swipe-to-delete functionality
 }
 
 extension BudgetFormController: UITextFieldDelegate{
